f08c57f632914e110b878d2a95356496c60e171b
Alice Hofverberg <ali02@edu.umea.se>
2026-09-01 11:51:17 +0200

    uppdaterad version 1 september

--- slump.py
+++ slump.py
@@ -1,7 +1,7 @@
 import random
 
-ditt_nummer = random.randint (1, 1000)
-läskigaAI_nummer = random.randint (1, 1000)
+ditt_nummer = random.randint (1, 100)
+läskigaAI_nummer = random.randint (1, 100)
 
 name = input ("skriv ditt namn:")
 bett_summa = input("Hur mycket vill du betta?:")
@@ -11,9 +11,9 @@ print (f"du fick: {ditt_nummer}")
 print (f"Läskiga AI fick: {läskigaAI_nummer}")
 
 if ditt_nummer > läskigaAI_nummer:
-    print("Grattis" + name + "!" + "du vann") 
-    print(bett - 100)
-else: 
+    print("Grattis " +  name  + "!" + " du vann") 
+    print(bett + 100)
+if läskigaAI_nummer > ditt_nummer:
     print("Läskiga AI vann :(, nu har du") 
-    print(bett -100) 
+    print ("0") 
 
